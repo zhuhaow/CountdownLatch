b21d8e2797a250f9335790a95905e24d9212a23b
Zhuhao Wang <zhuhaow@gmail.com>
2016-08-06 23:10:03 +0800

    Initial Commit

---
+++ CountdownLatch/CountdownLatch.h
@@ -0,0 +1,19 @@
+//
+//  CountdownLatch.h
+//  CountdownLatch
+//
+//  Created by Zhuhao Wang on 8/6/16.
+//  Copyright © 2016 Zhuhao Wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CountdownLatch.
+FOUNDATION_EXPORT double CountdownLatchVersionNumber;
+
+//! Project version string for CountdownLatch.
+FOUNDATION_EXPORT const unsigned char CountdownLatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountdownLatch/PublicHeader.h>
+
+
